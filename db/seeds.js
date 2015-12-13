eda8c69688c377150184d4d7c7735dd798154a20
Rob Wilson <robwilson101@gmail.com>
2015-12-13 21:15:56 +0000

    Added demoLesson to a seed file

---
+++ db/seeds.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+var config = require('../config/config');
+var Lesson = require('../models/lesson');
+
+mongoose.connect(config.database);
+
+var demoLesson = new Lesson({
+  title: 'Demo Lesson',
+  content: '1. Write an immediatly invoked functional expression that prints "C0FF33C0D3R R0CK5!" to the screen.',
+  difficulty: 2
+});
+
+demoLesson.save(function (err, demoLesson) {
+  if (err) return console.log('[ERROR] - ' + err);
+  console.log('[+] - ' + title + ' added. Lesson content:\n ' + content);
+});
