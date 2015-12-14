e8c773b0e800cfc762b7b70e3177d5eb743134e4
phobos101 <robwilson101@gmail.com>
2015-12-14 11:27:06 +0000

    Added tests for users

--- test/lessonController_test.js
+++ test/lessonController_test.js
@@ -53,6 +53,16 @@ describe('GET /lessons', function() {
       });
   });
 
+  it('An inner object should contain an "expectedResult" property', function(done) {
+    api
+      .get('/lessons')
+      .set('Accept', 'application/json')
+      .end(function(err, res) {
+        expect(res.body.lessons[0]).to.have.property('expectedResult');
+        done();
+      });
+  });
+
 });
 
 describe('/GET users', function() {
@@ -62,6 +72,68 @@ describe('/GET users', function() {
       .get('/users')
       .set('Accept', 'application/json')
       .expect(200, done);
+  });
+
+  it('Should return an object containing an array of objects', function(done) {
+    api
+      .get('/users')
+      .set('Accept', 'application/json')
+      .end(function(err, res) {
+        expect(res.body).to.be.an('object');
+        expect(res.body.users).to.be.an('array');
+        expect(res.body.users[0]).to.be.an('object');
+        done();
+      });
+  });
+
+  it('An inner object should contain an "email" property', function(done) {
+    api
+      .get('/users')
+      .set('Accept', 'application/json')
+      .end(function(err, res) {
+        expect(res.body.users[0]).to.have.property('email');
+        done();
+      });
+  });
+
+  it('An inner object should contain a "password" property', function(done) {
+    api
+      .get('/users')
+      .set('Accept', 'application/json')
+      .end(function(err, res) {
+        expect(res.body.users[0]).to.have.property('password');
+        done();
+      });
+  });
+
+  it('An inner object should contain a "lessonsSubbed" property', function(done) {
+    api
+      .get('/users')
+      .set('Accept', 'application/json')
+      .end(function(err, res) {
+        expect(res.body.users[0]).to.have.property('lessonsSubbed');
+        done();
+      });
+  });
+
+  it('An inner object should contain a "lessonsCreated" property', function(done) {
+    api
+      .get('/users')
+      .set('Accept', 'application/json')
+      .end(function(err, res) {
+        expect(res.body.users[0]).to.have.property('lessonsCreated');
+        done();
+      });
+  });
+
+  it('An inner object should contain a "lessonsCompleted" property', function(done) {
+    api
+      .get('/users')
+      .set('Accept', 'application/json')
+      .end(function(err, res) {
+        expect(res.body.users[0]).to.have.property('lessonsCompleted');
+        done();
+      });
   });
 
 });
