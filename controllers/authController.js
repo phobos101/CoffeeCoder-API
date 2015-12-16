c484a2ecc812d27171ada6599a302e010f316134
phobos101 <robwilson101@gmail.com>
2015-12-16 14:52:15 +0000

    Secured the JWT

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,7 +10,7 @@ function register(req, res, next) {
     if (!user) return res.status(409).json({message: 'User already exists!'});
 
     // User has authenticated so issue token
-    var token = jwt.sign(user, secret, {expiresIn: 60 * 60 * 24});
+    var token = jwt.sign(user._id, secret, {expiresIn: 60 * 60 * 24});
 
     // Send back the token to the front-end to store
     return res.status(200).json({
@@ -29,8 +29,7 @@ function login(req, res, next) {
     if (err) return res.status(500).json(err);
     if (!user) return res.status(403).json({message: 'No account found with provided credentials.'});
     if (!user.validPassword(req.body.password)) return res.status(403).json({message: 'Authentication failed.'});
-
-    var token = jwt.sign(user, secret, {expiresIn: 60 * 60 * 24});
+    var token = jwt.sign(user._id, secret, {expiresIn: 60 * 60 * 24});
 
     return res.status(200).json({
       success: true,
