a7317584a963ac5301b821c3801ba7f0dfbc49ea
phobos101 <robwilson101@gmail.com>
2015-12-16 12:16:23 +0000

    Fixed method to allow updating

--- controllers/userController.js
+++ controllers/userController.js
@@ -23,9 +23,9 @@ function updateUser(req, res) {
   User.findById({_id: id}, function(err, user) {
     if (err) return res.status(500).json({message: "Something went wrong."});
     if (!user) return res.status(204).json({message: 'No user found with provided ID.'});
-
-    if (req.body.email) user.local.email = req.body.email;
-    if (req.body.password) user.local.password = req.body.password;
+    console.log(req.body.local)
+    if (req.body.local.email) user.local.email = req.body.local.email;
+    // if (req.body.local.password) user.local.password = req.body.local.password;
     if (req.body.lessonsSubbed) user.lessonsSubbed = req.body.lessonsSubbed;
     if (req.body.lessonsCreated) user.lessonsCreated = req.body.lessonsCreated;
     if (req.body.lessonsCompleted) user.lessonsCompleted = req.body.lessonsCompleted;
